ca67457ec2ebdd23a704e14be0e5299f28b5f331
Di Zheng <dizhengcindy@gmail.com>
2020-02-27 00:26:43 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,30 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(names)
+  names.each_with_index {|element, index|
+    puts "#{index+1}.* #{element}"
+  }
+  
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(calls)
+  calls.map{|word|
+    word.capitalize + "!"
+  }
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(calls)
+  calls.any?{|word|
+  word.length >4
+  }
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(aos)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  
+  result = aos.find{|element|
+    element.include?(cheese_types[0]) || element.include?(cheese_types[1]) || 
+    element.include?(cheese_types[2])
+  }
+  result
+  
 end
